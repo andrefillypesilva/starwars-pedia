7cd4928d9ffc9bb3b1b57226f15bde803db81e58
André Fillype <andrefillype10@gmail.com>
2020-11-11 13:44:24 +0000

    Implementing search (only Films)

--- src/app/resources/components/result-search/result-search.component.ts
+++ src/app/resources/components/result-search/result-search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
+import { Film } from 'src/app/models/interfaces/film';
 import { ResultCard } from '../../../models/interfaces/result-card';
 import { SearchService } from '../../services/search.service';
 import { FilmDetailModalComponent } from '../films/film-detail-modal/film-detail-modal.component';
@@ -19,27 +20,28 @@ export class ResultSearchComponent implements OnInit {
     private readonly activatedRoute: ActivatedRoute,
     private readonly searchService: SearchService,
     private readonly modalService: NgbModal
-  ) {
-    activatedRoute.queryParams.subscribe((params: Params) => {
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
       this.search = params.search;
+      this.results$ = this.searchService.search(this.search);
     });
   }
 
-  ngOnInit(): void {
-    this.results$ = this.searchService.search(this.search);
-  }
-
-  onOpenResult(): void {
-    const modal = this.modalService.open(FilmDetailModalComponent);
-    modal.componentInstance.object = {
-      title: 'A New Hope',
-      episode_id: 4,
-      opening_crawl: 'Lorem ipsum dolor sit amet',
-      director: 'André Fillype',
-      producer: 'Fillype André',
-      release_date: '26/09/1994',
-      url: 'http://www.google.com'
-    };
+  onOpenResult(id: number): void {
+    this.searchService.getResultById(id).subscribe((result: Film) => {
+      const modal = this.modalService.open(FilmDetailModalComponent);
+      modal.componentInstance.object = {
+        title: result.title,
+        episode_id: result.episode_id,
+        opening_crawl: result.opening_crawl,
+        director: result.director,
+        producer: result.producer,
+        release_date: result.release_date,
+        url: result.url
+      };
+    });
   }
 
 }
--- src/app/resources/services/search.service.ts
+++ src/app/resources/services/search.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ResultCard } from 'src/app/models/interfaces/result-card';
 import { Observable } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Film } from 'src/app/models/interfaces/film';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +17,22 @@ export class SearchService {
 
   search(search: string): Observable<ResultCard[]> {
     const queryParams = new HttpParams().append('search', search);
-    return this.http.get<ResultCard[]>('assets/mocks/results.json', { params: queryParams });
+    return this.http.get<ResultCard[]>(`${environment.swapi}films`, { params: queryParams })
+    .pipe(
+      pluck('results'),
+      map((results: any[]) => {
+        return results.map((result: Film) => ({
+          id: parseInt(result.url.split('/')[5], 10),
+          title: result.title,
+          description: result.opening_crawl,
+          options: [result.director, result.producer],
+          url: result.url
+        }));
+      })
+    );
+  }
+
+  getResultById(id: number): Observable<Film> {
+    return this.http.get<Film>(`${environment.swapi}films/${id}`);
   }
 }
