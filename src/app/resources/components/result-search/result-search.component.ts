c5de146206929aaa18600a4ee5dd433406b541ba
André Fillype <andrefillype10@gmail.com>
2020-11-11 00:15:48 +0000

    Style small fixes

--- src/app/resources/components/result-search/result-search.component.ts
+++ src/app/resources/components/result-search/result-search.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { ResultCard } from '../../../models/interfaces/result-card';
 import { SearchService } from '../../services/search.service';
+import { FilmDetailModalComponent } from '../films/film-detail-modal/film-detail-modal.component';
 
 @Component({
   selector: 'app-result-search',
@@ -15,7 +17,8 @@ export class ResultSearchComponent implements OnInit {
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
-    private readonly searchService: SearchService
+    private readonly searchService: SearchService,
+    private readonly modalService: NgbModal
   ) {
     activatedRoute.queryParams.subscribe((params: Params) => {
       this.search = params.search;
@@ -24,6 +27,19 @@ export class ResultSearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.results$ = this.searchService.search(this.search);
+  }
+
+  onOpenResult(): void {
+    const modal = this.modalService.open(FilmDetailModalComponent);
+    modal.componentInstance.object = {
+      title: 'A New Hope',
+      episode_id: 4,
+      opening_crawl: 'Lorem ipsum dolor sit amet',
+      director: 'André Fillype',
+      producer: 'Fillype André',
+      release_date: '26/09/1994',
+      url: 'http://www.google.com'
+    };
   }
 
 }
