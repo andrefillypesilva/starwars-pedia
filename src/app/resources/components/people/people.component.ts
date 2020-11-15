22b25700e95a355fbf7083c305285481f82564fd
André Fillype <andrefillype10@gmail.com>
2020-11-15 23:17:20 +0000

    Preparing missing-people-endpoint-implementation branch

--- src/app/resources/components/people/people.component.ts
+++ src/app/resources/components/people/people.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Observable } from 'rxjs';
-import { ResultCard } from 'src/app/models/interfaces/result-card';
-import { PersonService } from '../../services/person.service';
-import { PersonDetailModalComponent } from './person-detail-modal/person-detail-modal.component';
 
 @Component({
   selector: 'app-people',
@@ -11,30 +6,12 @@ import { PersonDetailModalComponent } from './person-detail-modal/person-detail-
 })
 export class PeopleComponent implements OnInit {
 
-  public people$: Observable<ResultCard[]>;
+  constructor() { }
 
-  constructor(
-    private readonly personService: PersonService,
-    private readonly modalService: NgbModal
-  ) { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-    this.people$ = this.personService.getPeople();
-  }
+  onOpenPerson(): void {
 
-  onOpenPerson(id: number): void {
-    const modal = this.modalService.open(PersonDetailModalComponent);
-    modal.componentInstance.object = {
-      name: 'Luke Skywalker',
-      birth_year: '26/09/1994',
-      eye_color: 'Brown',
-      gender: 'Male',
-      hair_color: 'Black',
-      height: '1.73',
-      mass: '75kg',
-      skin_color: 'Brown',
-      homeworld: 'Earth'
-    };
   }
 
 }
