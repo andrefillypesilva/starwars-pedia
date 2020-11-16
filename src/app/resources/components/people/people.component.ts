b5a9ed7504f770ecc46c21ebf4e35f393cc552fd
André Fillype <andrefillype10@gmail.com>
2020-11-16 14:44:34 +0000

    People endpoint implementation

--- src/app/resources/components/people/people.component.ts
+++ src/app/resources/components/people/people.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+import { Person } from 'src/app/models/interfaces/person';
+import { ResultCard } from 'src/app/models/interfaces/result-card';
+import { PersonService } from '../../services/person.service';
+import { PersonDetailModalComponent } from './person-detail-modal/person-detail-modal.component';
 
 @Component({
   selector: 'app-people',
@@ -6,12 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleComponent implements OnInit {
 
-  constructor() { }
+  public people$: Observable<ResultCard[]>;
 
-  ngOnInit(): void { }
+  constructor(
+    private readonly personService: PersonService,
+    private readonly modalService: NgbModal
+  ) { }
+
+  ngOnInit(): void {
+    this.people$ = this.personService.getPeople();
+  }
 
   onOpenPerson(id: number): void {
-
+    this.personService.getPersonById(id).subscribe((person: Person) => {
+      const modal = this.modalService.open(PersonDetailModalComponent);
+      modal.componentInstance.object = {
+        name: person.name,
+        birth_year: person.birth_year,
+        eye_color: person.eye_color,
+        gender: person.gender,
+        hair_color: person.hair_color,
+        height: person.height,
+        mass: person.mass,
+        skin_color: person.skin_color,
+        homeworld: person.homeworld
+      };
+    });
   }
 
 }
--- src/app/resources/services/person.service.ts
+++ src/app/resources/services/person.service.ts
@@ -1,10 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
+import { Person } from 'src/app/models/interfaces/person';
+import { ResultCard } from 'src/app/models/interfaces/result-card';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  getPeople(): Observable<ResultCard[]> {
+    return this.http.get<ResultCard[]>(`${environment.swapi}people`)
+    .pipe(
+      pluck('results'),
+      map((people: any[]) => {
+        return people.map((person: Person) => ({
+          id: parseInt(person.url.split('/')[5], 10),
+          title: `${person.name}`,
+          description: `From: ${person.homeworld} - Birth Year: ${person.birth_year}`,
+          options: [`Skin Color: ${person.skin_color}`, `Hair Color: ${person.hair_color}`, `Eye Color: ${person.eye_color}`],
+          url: person.url,
+        }));
+      })
+    );
+  }
+
+  getPersonById(id: number): Observable<Person> {
+    return this.http.get<Person>(`${environment.swapi}people/${id}`);
+  }
 }
